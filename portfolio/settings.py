eb1bb7f6adf8a8f411bcc67c6025900c5667accd
Aryan Pawar <aryanpawar049120@gmail.com>
2026-06-27 12:26:13 +0530

    Fix: Email respond

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,8 +123,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-# Securely reading BOTH email and app password from environment variables
-EMAIL_HOST_USER = os.getenv('DJANGO_EMAIL_USER')
-DEFAULT_FROM_EMAIL = os.getenv('DJANGO_EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.getenv('HOST_PASSWORD')
